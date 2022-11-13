cf600d15f27ad17724220984ab9efc429bf8d92b
Karina Moreira <kakatinosa@hotmail.com>
2022-11-13 14:08:26 -0300

    Export tool report to Excel sheet

--- Application/Controllers/AnaliseDadosController.cs
+++ Application/Controllers/AnaliseDadosController.cs
@@ -2,7 +2,9 @@
 using Domain.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OfficeOpenXml;
 using System;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -82,6 +84,61 @@ namespace Application.Controllers
                 var msgErro = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
                 _logger.LogError(ex, msgErro);
                 return BadRequest($"Ocorreu um erro ao gerar o relatório de empréstimos:\n{msgErro}");
+            }
+        }
+
+        /// <summary>
+        /// Exporta planilha de relatório de ferramentas.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("PlanilhaRelatorioFerramentas")]
+        [ProducesResponseType(typeof(File), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> ExportRelatorioFerramentas()
+        {
+            try
+            {
+                var stream = new MemoryStream();
+
+                using (var ep = new ExcelPackage(stream))
+                {
+                    ep.Workbook.Properties.Author = "Ferramentaria FURNAS";
+                    ep.Workbook.Properties.Title = "Relatório Ferramentaria";
+                    ep.Workbook.Properties.Subject = "Análise de Dados de Ferramentas";
+                    ep.Workbook.Properties.Created = DateTime.Now;
+                    ep.Workbook.Date1904 = true;
+
+                    ExcelWorksheet p1 = ep.Workbook.Worksheets.Add("Ferramentas");
+
+                    var relatorio = await _analiseDadosService.GerarRelatorioFerramentas();
+
+                    p1.Column(1).Style.Font.Bold = true;
+                    p1.Column(1).Width = 35;
+
+                    p1.Cells[1, 1].Value = "Quantidade Total";
+                    p1.Cells[2, 1].Value = "Quantidade Disponível";
+                    p1.Cells[3, 1].Value = "Quantidade Emprestada";
+                    p1.Cells[4, 1].Value = "Média Quantidade por Ferramenta";
+                    p1.Cells[5, 1].Value = "Quantidade Inativas";
+
+                    p1.Cells[1, 2].Value = relatorio.QuantidadeTotal;
+                    p1.Cells[2, 2].Value = relatorio.QuantidadeDisponivel;
+                    p1.Cells[3, 2].Value = relatorio.QuantidadeEmprestada;
+                    p1.Cells[4, 2].Value = relatorio.MediaQuantidadePorFerramenta;
+                    p1.Cells[5, 2].Value = relatorio.QuantidadeInativas;
+
+                    ep.Save();
+                };
+
+                stream.Position = 0;
+                string excelName = $"RelatorioFerramentas-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
+            }
+            catch (Exception ex)
+            {
+                var msgErro = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                _logger.LogError(ex, msgErro);
+                return BadRequest($"Ocorreu um erro ao exportar o relatório de ferramentas:\n{msgErro}");
             }
         }
     }
